729a81271c746d40e8d8b6c4bfb4571f39393906
artemiosdev <flyboroda@gmail.com>
2022-11-25 10:39:21 +0300

    Add the new app UIPageView

---
+++ UIPageView/UIPageView/ContentViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ContentViewController.swift
+//  UIPageView
+//
+//  Created by Artem Androsenko on 25.11.2022.
+//
+
+import UIKit
+
+class ContentViewController: UIViewController {
+    @IBOutlet weak var presentationTextLabel: UILabel!
+    @IBOutlet weak var emojiLabel: UILabel!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ UIPageView/UIPageView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIPageView
+//
+//  Created by Artem Androsenko on 25.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
