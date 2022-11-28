4f755b7096e8077ba574424c931d17ad65c5676c
artemiosdev <flyboroda@gmail.com>
2022-11-28 13:31:52 +0300

    Закончил разработку приложения UIGestureRecognizer, добавил функционал распознавания и отображения swipes and tapes

--- UIGestureRecognizer/UIGestureRecognizer/ViewController.swift
+++ UIGestureRecognizer/UIGestureRecognizer/ViewController.swift
@@ -8,19 +8,64 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        swipeObservers()
+        tapObservers()
     }
     
     func swipeObservers() {
+        // создаем наблюдателей для каждого свайпа
         let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes))
         swipeRight.direction = .right
+        self.view.addGestureRecognizer(swipeRight)
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes))
+        swipeLeft.direction = .left
+        self.view.addGestureRecognizer(swipeLeft)
+        
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes))
+        swipeUp.direction = .up
+        self.view.addGestureRecognizer(swipeUp)
+        
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes))
+        swipeDown.direction = .down
+        self.view.addGestureRecognizer(swipeDown)
+    }
+    func tapObservers() {
+        // создаем наблюдателей для каждого тапа
+        let tripleTap = UITapGestureRecognizer(target: self, action: #selector(tripleTapAction))
+        // кол-во отслеживаемых тапов
+        tripleTap.numberOfTapsRequired = 3
+        self.view.addGestureRecognizer(tripleTap)
+        
+        let doubleTap = UITapGestureRecognizer(target: self, action: #selector(doubleTapAction))
+        // для того чтобы проигнорировать двойной тап, если тапаем трижды
+        doubleTap.require(toFail: tripleTap)
+        doubleTap.numberOfTapsRequired = 2
+        self.view.addGestureRecognizer(doubleTap )
     }
     
     @objc func handleSwipes(gester: UISwipeGestureRecognizer) {
-        
+        switch gester.direction {
+        case .right:
+            label.text = "Right swipe was recognized"
+        case .left:
+            label.text = "Left swipe was recognized"
+        case .up:
+            label.text = "Up swipe was recognized"
+        case .down:
+            label.text = "Down swipe was recognized"
+        default:
+            break
+        }
+    }
+    @objc func tripleTapAction() {
+        label.text = "Triple taped was recognized"
+    }
+    @objc func doubleTapAction() {
+        label.text = "Double taped was recognized"
     }
 }
 
