17008d70d85acbb31202c6c23a35716508f0fcfa
artemiosdev <flyboroda@gmail.com>
2023-01-25 14:55:18 +0300

    Обязательные методы для TableView, Navigation Controller, Bar

--- EmojiReader/EmojiReader/EmojiTableViewController.swift
+++ EmojiReader/EmojiReader/EmojiTableViewController.swift
@@ -16,30 +16,36 @@ class EmojiTableViewController: UITableViewController {
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        self.title = "Emoji Reader"
+        self.navigationItem.leftBarButtonItem = self.editButtonItem
+        
+        // зарегистрируем новую ячейку
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 5
     }
 
-    /*
+ 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        // устаревшее свойство
+        // cell.textLabel?.text = "\(indexPath)"
+        content.text = "\(indexPath)"
+        cell.contentConfiguration = content
         return cell
     }
-    */
+   
 
     /*
     // Override to support conditional editing of the table view.
