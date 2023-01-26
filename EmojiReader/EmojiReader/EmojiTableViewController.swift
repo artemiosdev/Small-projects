30e5f3c65f0c5a567250437303ca9916d34d7301
artemiosdev <flyboroda@gmail.com>
2023-01-26 23:55:10 +0300

    Добавил 2 кнопки по свайпу на ячейке, удаление и лайк

--- EmojiReader/EmojiReader/EmojiTableViewController.swift
+++ EmojiReader/EmojiReader/EmojiTableViewController.swift
@@ -76,4 +76,36 @@ class EmojiTableViewController: UITableViewController {
         objects.insert(moveEmoji, at: destinationIndexPath.row)
         tableView.reloadData()
     }
+    
+    // добавим кнопку по свайпу влево, по ячейке
+    override func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let done = doneAction(at: indexPath)
+        let favourite = favouriteAction(at: indexPath)
+        return UISwipeActionsConfiguration(actions: [done, favourite])
+    }
+    
+    // функционал удаления для кнопки
+    func doneAction(at indexPath: IndexPath) -> UIContextualAction {
+        let action = UIContextualAction(style: .destructive, title: "Done") { (action, view, completion) in
+            self.objects.remove(at: indexPath.row)
+            self.tableView.deleteRows(at: [indexPath], with: .automatic)
+            completion(true)
+        }
+        action.backgroundColor = .systemGreen
+        action.image = UIImage(systemName: "checkmark.circle")
+        return action
+    }
+    
+    // функционал для like для кнопки
+    func favouriteAction(at indexPath: IndexPath) -> UIContextualAction {
+        var object = objects[indexPath.row]
+        let action = UIContextualAction(style: .normal, title: "Favourite") { (action, view, completion) in
+            object.isFavourite = !object.isFavourite
+            self.objects[indexPath.row] = object
+            completion(true)
+        }
+        action.backgroundColor = object.isFavourite ? .systemPurple : .systemGray
+        action.image = UIImage(systemName: "heart")
+        return action
+    }
 }
