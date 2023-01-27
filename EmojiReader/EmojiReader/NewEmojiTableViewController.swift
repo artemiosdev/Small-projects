6c91f740383eeafa6dfea885ebe696a0213d366b
artemiosdev <flyboroda@gmail.com>
2023-01-27 12:21:17 +0300

    Setup Second Screen Interface. Создал новый класс NewEmojiTableViewController для нового экрана добавления и редактирования ячейки

---
+++ EmojiReader/EmojiReader/NewEmojiTableViewController.swift
@@ -0,0 +1,27 @@
+//
+//  NewEmojiTableViewController.swift
+//  EmojiReader
+//
+//  Created by Artem Androsenko on 27.01.2023.
+//
+
+import UIKit
+
+class NewEmojiTableViewController: UITableViewController {
+    @IBOutlet weak var emojiTextField: UITextField!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var descriptionTextField: UITextField!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    @IBAction func textChanged(_ sender: UITextField) {
+    }
+    
+
+
+    
+
+}
