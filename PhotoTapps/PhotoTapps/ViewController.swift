b11c9dc22b679d88a07ab71c122ee19b6d86bba6
artemiosdev <flyboroda@gmail.com>
2023-01-28 23:36:52 +0300

    Новое приложение PhotoTapps построенное на Collection View Controller

---
+++ PhotoTapps/PhotoTapps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoTapps
+//
+//  Created by Artem Androsenko on 28.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
