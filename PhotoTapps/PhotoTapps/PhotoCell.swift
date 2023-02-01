4e6c529053dbd61dbfb1d0a0bf9060083663a941
artemiosdev <flyboroda@gmail.com>
2023-02-01 16:21:17 +0300

    Добавил картинки, настроил отображение ячеек

--- PhotoTapps/PhotoTapps/PhotoCell.swift
+++ PhotoTapps/PhotoTapps/PhotoCell.swift
@@ -8,5 +8,5 @@
 import UIKit
 
 class PhotoCell: UICollectionViewCell {
-    
+    @IBOutlet weak var dogImageView: UIImageView!
 }
--- PhotoTapps/PhotoTapps/PhotosCollectionViewController.swift
+++ PhotoTapps/PhotoTapps/PhotosCollectionViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class PhotosCollectionViewController: UICollectionViewController {
-    let itemsPerRow: CGFloat = 2
+    let photos = ["dog1", "dog2", "dog3", "dog4", "dog5", "dog6", "dog7", "dog8", "dog9", "dog10", "dog11", "dog12", "dog13", "dog14", "dog15"]
+    let itemsPerRow: CGFloat = 3
     let sectionInserts = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
     
     override func viewDidLoad() {
@@ -40,14 +41,16 @@ class PhotosCollectionViewController: UICollectionViewController {
     // количество элементов в секции
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return 15
+        return photos.count
     }
 
     // конфигурация ячейки
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath) as! PhotoCell
         // Configure the cell
-        cell.backgroundColor = .blue
+        let imageName = photos[indexPath.item]
+        let image = UIImage(named: imageName)
+        cell.dogImageView.image = image
         return cell
     }
 
