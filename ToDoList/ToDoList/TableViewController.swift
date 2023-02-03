ea31178578da9532ec7dd58dd375e81ad15b8985
artemiosdev <flyboroda@gmail.com>
2023-02-03 23:47:42 +0300

    Добавил функционал таблицы, actions для добавления новой ячейки-задачи

---
+++ ToDoList/ToDoList/TableViewController.swift
@@ -0,0 +1,65 @@
+//
+//  TableViewController.swift
+//  ToDoList
+//
+//  Created by Artem Androsenko on 03.02.2023.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    var tasks: [String] = []
+    
+    @IBAction func saveTask(_ sender: UIBarButtonItem) {
+        let alertController = UIAlertController(title: "New Task", message: "Please add a new task", preferredStyle: .alert)
+        let saveAction = UIAlertAction(title: "Save", style: .default) { action in
+            let textField = alertController.textFields?.first
+            if let newTask = textField?.text {
+                // каждый новая задача будет на 1 месте
+                self.tasks.insert(newTask, at: 0)
+                // перезагружаем таблицу
+                self.tableView.reloadData()
+            }
+        }
+        alertController.addTextField { _ in }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default) { _ in }
+        alertController.addAction(saveAction)
+        alertController.addAction(cancelAction)
+        // отобразим наш alertController
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return tasks.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        content.text = tasks[indexPath.row]
+        content.secondaryText = "detail text"
+        cell.contentConfiguration = content
+
+        return cell
+    }
+
+
+}
