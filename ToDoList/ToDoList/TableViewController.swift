fb450143904d7e14319cd0b70588ef4614e7d265
artemiosdev <flyboroda@gmail.com>
2023-02-05 23:56:29 +0300

    Реализовал добавление и удаление данных из Core Data

--- ToDoList/ToDoList/TableViewController.swift
+++ ToDoList/ToDoList/TableViewController.swift
@@ -29,15 +29,42 @@ class TableViewController: UITableViewController {
         present(alertController, animated: true, completion: nil)
     }
     
-    // save info in Core Data
-    private func saveTask(withTitle title: String) {
+    // метод вызовающий Context
+    private func getContext() ->  NSManagedObjectContext {
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        let context = appDelegate.persistentContainer.viewContext
+        return appDelegate.persistentContainer.viewContext
+    }
+    
+    
+    // сохраним данные in Core Data
+    private func saveTask(withTitle title: String) {
+        let context = getContext()
         guard let entity = NSEntityDescription.entity(forEntityName: "Task", in: context) else { return }
         let taskObject = Task(entity: entity, insertInto: context)
         taskObject.title = title
         do {
             try context.save()
+            tasks.append(taskObject)
+            
+            // чтобы новая задача была всегда первой по счету
+            // tasks.insert(taskObject, at: 0)
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+    }
+    
+    // загрузим хранимые данные из Core Data на экран
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let context = getContext()
+        // запрос на получение хранящийся объектов
+        let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
+        // сортировка по названию, возрастание/убывание
+        let sortDescriptor = NSSortDescriptor(key: "title", ascending: false)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        
+        do {
+            tasks = try context.fetch(fetchRequest)
         } catch let error as NSError {
             print(error.localizedDescription)
         }
@@ -45,26 +72,37 @@ class TableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        // удалим данные из Core Data:
+        
+        let context = getContext()
+        // запрос на получение хранящийся объектов
+        let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
+        // пытаемся получить все результаты из Context
+        if let results = try? context.fetch(fetchRequest) {
+            for object in results {
+                context.delete(object)
+            }
+        }
+        do {
+            try context.save()
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+        //
+        
     }
-
+    
     // MARK: - Table view data source
-
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
+    // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
+    // #warning Incomplete implementation, return the number of rows
         return tasks.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         var content = cell.defaultContentConfiguration()
@@ -72,9 +110,7 @@ class TableViewController: UITableViewController {
         content.text = task.title
         content.secondaryText = "detail text"
         cell.contentConfiguration = content
-
         return cell
     }
-
-
+    
 }
