d7a4b03ae32b9aa8bdda1db297a8e149a7a63675
artemiosdev <flyboroda@gmail.com>
2023-02-05 11:28:10 +0300

    Добавил реализацию сохранения данных в CoreData

--- ToDoList/ToDoList/TableViewController.swift
+++ ToDoList/ToDoList/TableViewController.swift
@@ -6,17 +6,17 @@
 //
 
 import UIKit
+import CoreData
 
 class TableViewController: UITableViewController {
-    var tasks: [String] = []
+    var tasks: [Task] = []
     
     @IBAction func saveTask(_ sender: UIBarButtonItem) {
         let alertController = UIAlertController(title: "New Task", message: "Please add a new task", preferredStyle: .alert)
         let saveAction = UIAlertAction(title: "Save", style: .default) { action in
             let textField = alertController.textFields?.first
-            if let newTask = textField?.text {
-                // каждый новая задача будет на 1 месте
-                self.tasks.insert(newTask, at: 0)
+            if let newTaskTitle = textField?.text {
+                self.saveTask(withTitle: newTaskTitle)
                 // перезагружаем таблицу
                 self.tableView.reloadData()
             }
@@ -27,6 +27,20 @@ class TableViewController: UITableViewController {
         alertController.addAction(cancelAction)
         // отобразим наш alertController
         present(alertController, animated: true, completion: nil)
+    }
+    
+    // save info in Core Data
+    private func saveTask(withTitle title: String) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        guard let entity = NSEntityDescription.entity(forEntityName: "Task", in: context) else { return }
+        let taskObject = Task(entity: entity, insertInto: context)
+        taskObject.title = title
+        do {
+            try context.save()
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
     }
     
     override func viewDidLoad() {
@@ -54,7 +68,8 @@ class TableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         var content = cell.defaultContentConfiguration()
-        content.text = tasks[indexPath.row]
+        let task = tasks[indexPath.row]
+        content.text = task.title
         content.secondaryText = "detail text"
         cell.contentConfiguration = content
 
