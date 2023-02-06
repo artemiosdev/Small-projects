d7937d3b310230612eb795b975e1c2cc3c29066d
artemiosdev <flyboroda@gmail.com>
2023-02-06 13:48:41 +0300

    Добавил новый проект MyCars

---
+++ MyCars/MyCars/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  MyCars
+//
+//  Created by Artem Androsenko on 06.02.2023.
+//
+
+import UIKit
+import CoreData
+
+class ViewController: UIViewController {
+    
+    lazy var context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var markLabel: UILabel!
+    @IBOutlet weak var modelLabel: UILabel!
+    @IBOutlet weak var carImageView: UIImageView!
+    @IBOutlet weak var lastTimeStartedLabel: UILabel!
+    @IBOutlet weak var numberOfTripsLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var myChoiceImageView: UIImageView!
+    
+    @IBAction func segmentedCtrlPressed(_ sender: UISegmentedControl) {
+        
+    }
+    
+    @IBAction func startEnginePressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func rateItPressed(_ sender: UIButton) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+}
+
