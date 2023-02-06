0f523c91c7e77be1d191793ff126fe8c1e9b219e
artemiosdev <flyboroda@gmail.com>
2023-02-06 21:56:42 +0300

    Вставляем наши данные в интерфейс приложения

--- MyCars/MyCars/ViewController.swift
+++ MyCars/MyCars/ViewController.swift
@@ -12,6 +12,14 @@ class ViewController: UIViewController {
     
     var context: NSManagedObjectContext!
     
+    // определим формат даты
+    lazy var dateFormatter: DateFormatter = {
+        let df = DateFormatter()
+        df.dateStyle = .short
+        df.timeStyle = .none
+        return df
+    }()
+    
     @IBOutlet weak var segmentedControl: UISegmentedControl!
     @IBOutlet weak var markLabel: UILabel!
     @IBOutlet weak var modelLabel: UILabel!
@@ -33,12 +41,26 @@ class ViewController: UIViewController {
         
     }
     
+    // распределим полученные данные по нашему интерфейсу
+    private func insertDataFrom(selectedCar car: Car) {
+        carImageView.image = UIImage(data: car.imageData!)
+        markLabel.text = car.mark
+        modelLabel.text = car.model
+        myChoiceImageView.isHidden = !(car.myChoice)
+        ratingLabel.text = "Rating: \(car.rating) / 10"
+        numberOfTripsLabel.text = "Number of trips: \(car.timesDriven)"
+        
+        lastTimeStartedLabel.text = "Last time started: \(dateFormatter.string(from: car.lastStarted!))"
+        segmentedControl.tintColor = car.tintColor as? UIColor
+    }
+    
     private func getDataFromFile() {
         // если в базе у нас есть записи, то код не будет выполнен ниже
         // если нет, то выполнение осуществится и данные попадут в базу Core Data
         let fetchRequest: NSFetchRequest<Car> = Car.fetchRequest()
         // мы хотим получить записи которые есть и не равны nil
         fetchRequest.predicate = NSPredicate(format: "mark != nil")
+        
         var records = 0
         
         do {
@@ -53,7 +75,7 @@ class ViewController: UIViewController {
         
         // получаем массив значений из нашего файла data.plist
         guard let pathToFile = Bundle.main.path(forResource: "data", ofType: "plist"),
-                let dataArray = NSArray(contentsOfFile: pathToFile) else { return }
+              let dataArray = NSArray(contentsOfFile: pathToFile) else { return }
         
         // поместим данные из массива data.plist в Core Data
         // пробегаемся по каждому словарю который запакован в массив
@@ -63,24 +85,21 @@ class ViewController: UIViewController {
             let car = NSManagedObject(entity: entity!, insertInto: context) as! Car
             // все свойства помещаем в нашу сущность car
             let carDictionary = dictionary as! [String : AnyObject]
-            car.model = carDictionary["mark"] as? String
+            car.mark = carDictionary["mark"] as? String
             car.model = carDictionary["model"] as? String
             car.rating = carDictionary["rating"] as! Double
             car.lastStarted = carDictionary["lastStarted"] as? Date
             car.timesDriven = carDictionary["timesDriven"] as! Int16
             car.myChoice = carDictionary["myChoice"] as! Bool
             
-            guard let imageName = carDictionary["imageData"] as? String else { return }
-            guard let image = UIImage(named: imageName) else { return }
-            let imageData = image.pngData()
+            let imageName = carDictionary["imageName"] as? String
+            let image = UIImage(named: imageName!)
+            let imageData = image!.pngData()
             car.imageData = imageData
             
             if let colorDictionary = carDictionary["tintColor"] as? [String : Float] {
                 car.tintColor = getColor(colorDictionary: colorDictionary)
             }
-            
-            
-            
         }
     }
     
@@ -93,8 +112,19 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         getDataFromFile()
+        
+        let fetchRequest: NSFetchRequest<Car> = Car.fetchRequest()
+        let mark = segmentedControl.titleForSegment(at: 0)
+        fetchRequest.predicate = NSPredicate(format: "mark == %@", mark!)
+        
+        do {
+            let results = try context.fetch(fetchRequest)
+            let car = results.first
+            insertDataFrom(selectedCar: car!)
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
     }
-    
 }
-
