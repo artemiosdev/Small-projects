e1d0c8633d61b9f571730c42db3b05d3f0efd30c
artemiosdev <flyboroda@gmail.com>
2023-02-07 16:53:33 +0300

    Добавил новое приложени-пример по CoreData, MealTime фиксирующее время приема пищи

---
+++ MealTime/MealTime/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  MealTime
+//
+//  Created by Artem Androsenko on 07.02.2023.
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var tableView: UITableView!
+    
+    var array = [Date]()
+    
+    lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .short
+        return formatter
+    }()
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        let date = Date()
+        array.append(date)
+        tableView.reloadData()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "My happy meal time"
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        
+        let date = array[indexPath.row]
+        
+        cell!.textLabel!.text = dateFormatter.string(from: date)
+        return cell!
+    }
+}
+
