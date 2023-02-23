1edef98d0ccb981c136765c52746f2b301907330
artemiosdev <flyboroda@gmail.com>
2023-02-24 00:12:33 +0300

    Добавил новое приложение Destini с выбором тестовых ответов

---
+++ Destini/Destini-iOS13/Controller/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Destini-iOS13
+//
+//  Created by Artem Androsenko on 24.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    
+    var storyBrain = StoryBrain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+
+    @IBAction func choiceMade(_ sender: UIButton) {
+        storyBrain.nextStory(userChoice: sender.currentTitle!)
+        updateUI()
+    }
+    
+    func updateUI() {
+        storyLabel.text = storyBrain.getStoryTitle()
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
+    }
+}
+
---
+++ Destini/Destini-iOS13/Model/Story.swift
@@ -0,0 +1,15 @@
+//
+//  Sotry.swift
+//  Destini-iOS13
+//
+//  Created by Artem Androsenko on 24.02.2022.
+//
+
+import Foundation
+struct Story {
+    let title: String
+    let choice1: String
+    let choice1Destination: Int
+    let choice2: String
+    let choice2Destination: Int
+}
