c297eadb877ba515d970b79bece0741d4c511cc3
artemiosdev <flyboroda@gmail.com>
2023-03-01 23:10:20 +0300

    Добавил новое приложение пример Quizzler с возможностью выбора ответа

---
+++ Quizzler/Quizzler/Model/Question.swift
@@ -0,0 +1,18 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Artem Androsenko on 1.03.2023.
+//
+
+import Foundation
+struct Question {
+    let text: String
+    let answer: String
+    
+    init (q: String, a: String) {
+        text = q
+        answer = a
+    }
+}
+
