84ff18341c669a2fa233c89f22b5f58bd61e525d
artemiosdev <flyboroda@gmail.com>
2023-06-17 22:48:39 +0300

    Добавил новый проект BMI_Calculator

---
+++ BMI_Calculator/BMI Calculator/Controllers/ResultViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ResultViewController.swift
+//  BMI Calculator
+//
+// Created by Androsenko Artem on 7.06.23
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    var bmiValue: String?
+    var advice: String?
+    var color: UIColor?
+
+    @IBOutlet weak var bmiLabel: UILabel!
+    @IBOutlet weak var adviceLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        bmiLabel.text = bmiValue
+        adviceLabel.text = advice
+        view.backgroundColor = color
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
---
+++ BMI_Calculator/BMI Calculator/Models/BMI.swift
@@ -0,0 +1,14 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+// Created by Androsenko Artem on 7.06.23
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
